9c1ed87a6ae975b0e68a2c536e982fd0b1433b96
Anoosragh A R <83896147+anoosragh69@users.noreply.github.com>
2024-04-22 13:54:25 +0530

    sky: Build GcamGo

--- device.mk
+++ device.mk
@@ -153,6 +153,10 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.camera.full.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.camera.full.xml \
     frameworks/native/data/etc/android.hardware.camera.raw.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.camera.raw.xml
 
+# CameraGo
+PRODUCT_PACKAGES += \
+    CameraGo
+
 # Capabilityconfigstore
 PRODUCT_PACKAGES += \
     vendor.qti.hardware.capabilityconfigstore@1.0.vendor

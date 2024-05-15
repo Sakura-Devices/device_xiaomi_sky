cf11fbda2145d835a90e0d74efe3938680a27089
Cole Faust <colefaust@google.com>
2024-05-15 09:40:32 +0000

    sky: Add BUILD_BROKEN_INCORRECT_PARTITION_IMAGES
    Qcom has issues that prevent it from working with a hermetic
    partition build.
    
    Bug: 205632228
    Test: Presubmits
    Change-Id: I490a29da38a6f68e7dd3650d8140cb11adaa8c7a

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -4,6 +4,8 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
+BUILD_BROKEN_INCORRECT_PARTITION_IMAGES := true
+
 # Inherit from the proprietary version
 -include vendor/xiaomi/sky/BoardConfigVendor.mk
 
